d25e79968a8df5da15217cd5091a3357aa4c9740
salmanyaseen2005 <salmanyaseen9090@gmai.com>
2023-12-10 03:17:05 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+alert("Welcome To JavaScript \n01 ")
+
+alert("Error! please enter a valid password \n02")
+
+alert("Welcome to JS Land \nHappy Coding!\n03")
+
+alert("Welcome to JS Land..\nQuestion 4")
+
+console.log("HELLO... I Can run JS through my web browser's Console\n05")
+
+// alert("06")
+
+alert("Practice placement of <script></script> element in following sections of your project in exercise 6:\n07")
+
